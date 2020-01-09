cbdf6fc5debd61d6d902d81ebdb47008be23c6f5
Smile Tom <utober77@gmail.com>
2020-01-09 19:53:54 +0900

    Updates

---
+++ precache-manifest.60f53a9bfd1d4edc3019133d547360c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd738e5182965f2f98fd7d99c7b925a",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "2b7976ad80a388323563",
+    "url": "/movie_app_2019/static/css/main.bd1cff42.chunk.css"
+  },
+  {
+    "revision": "2a913ea853a138080efd",
+    "url": "/movie_app_2019/static/js/2.914ea8a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.914ea8a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b7976ad80a388323563",
+    "url": "/movie_app_2019/static/js/main.1c16392d.chunk.js"
+  },
+  {
+    "revision": "d6fe1757db7d82fecf54",
+    "url": "/movie_app_2019/static/js/runtime-main.808adfd7.js"
+  }
+]);
